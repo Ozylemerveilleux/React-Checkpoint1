1dde51b7967866ce6ae823c9d502452909e59eb5
Ozylemerveilleux <nenebadj@gmail.com>
2021-12-28 06:35:12 -0800

    homework

---
+++ src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>HELLO SENEGAL</h1>
+        <p>Vous êtes priés de renseigner le formulaire suivant</p>
+      <h5>
+        <form>
+          <div>
+            <label>Nom</label>
+            <input/>
+          </div>
+          <br/>
+          <div>
+            <label>Prénom</label>
+            <input/>
+          </div>
+          <br/>
+          <div>
+            <label>Adresse</label>
+            <input/>
+          </div>
+          <br/>
+          <div>
+            <label>Fonction</label>
+            <input/>
+          </div>
+          <br/>
+          <div>
+            <label>Salaire</label>
+            <input/>
+          </div>
+        </form>
+      </h5>
+    </div>
+  );
+}
+
+export default App;
